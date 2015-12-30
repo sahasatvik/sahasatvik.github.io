9f4c5c1b39393b7368b75f9ae4dc325d5b5770da
sahasatvik <sahasatvik@gmail.com>
2015-12-30 19:16:00 +0530

    Added base files

---
+++ js/init.js
@@ -0,0 +1,10 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $(".dropdown-button").dropdown();
+    $('.parallax').parallax();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+

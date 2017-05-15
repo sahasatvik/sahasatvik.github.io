8ff121ee465b1053cc5dd78a7c418f7e80eeadcd
Satvik Saha <sahasatvik@gmail.com>
2017-05-15 23:18:53 +0530

    Updated list of quotes

--- quotes.js
+++ quotes.js
@@ -5,7 +5,7 @@ function showQuote() {
 			author: "Linus Torvalds"
 		},
 		{
-			text: "Talk is cheap. Show me the code.",
+			text: "Talk is cheap. Show me the code!",
 			author: "Linus Torvalds"
 		},
 		{
@@ -15,6 +15,42 @@ function showQuote() {
 		{
 			text: "Computers are good at following instructions, but not at reading your mind.",
 			author: "Donald Knuth"
+		},
+		{
+			text: "In order to understand recursion, one must first understand recursion.",
+			author: "Anonymous"
+		},
+		{
+			text: "Perl – The only language that looks the same before and after RSA encryption.",
+			author: "Keith Bostic"
+		},
+		{
+			text: "Pro·gram·mer - An organism capable of converting caffeine in to code.",
+			author: "Anonymous"
+		},
+		{
+			text: "For every complex problem there is an answer that is clear, simple, and wrong.",
+			author: "H L Mencken"
+		},
+		{
+			text: "Never attribute to malice that which is adequately explained by stupidity.",
+			author: "Hanlon's Razor'"
+		},
+		{
+			text: "Always code as if the guy who ends up maintaining your code will be a violent psychopath who knows where you live.",
+			author: "Martin Golding"
+		},
+		{
+			text: "Any fool can use a computer. Many do.",
+			author: "Ted Nelson"
+		},
+		{
+			text: "There is no place like 127.0.0.1",
+			author: "Anonymous"
+		},
+		{
+			text: "Simplicity is prerequisite for reliability.",
+			author: "Edsger W. Dijkstra"
 		}
 	]
 
